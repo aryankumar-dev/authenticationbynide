9d4bf4b7d03ec9aad8a9c65038e4ef0300b175ac
Aryan kumar <103895665+aryankumar-dev@users.noreply.github.com>
2025-03-24 22:03:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import express from "express";
 
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
 
+const port = process.env.PORT || 4000;
+
+app.get("/",(req,res) =>{
+    res.send("cohort");
+});
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`);
+});
